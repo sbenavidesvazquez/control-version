d876ddafecc14c0d2aa5e8646a473e62ce5b2485
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-01-26 09:28:08 +0100

    comit 3

--- src/pracversiones/PracVersiones.java
+++ src/pracversiones/PracVersiones.java
@@ -1,15 +1,28 @@
 
 package pracversiones;
 
+import java.util.Scanner;
+
 
 public class PracVersiones {
-
+    
+    
     
     public static void main(String[] args) {
         System.out.println("Practica de control de versiones ");
         System.out.println("version 1");
         System.out.println("Sergio benavides");
         
+        System.out.println("¿Que version escoje?");
+        Scanner dat=new Scanner(System.in);
+        int num=dat.nextInt();
+        
+        if(num==1){
+            System.out.println("version "+ num);
+        }
+        else{
+            System.out.println("no existe esa version");
+        }
     }
     
 }
