c60e4639ff7099d0f7d565f60fa0a9c3423bb40e
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-01-26 09:19:23 +0100

    comit 1

---
+++ src/pracversiones/PracVersiones.java
@@ -0,0 +1,13 @@
+
+package pracversiones;
+
+
+public class PracVersiones {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Practica de control de versiones ");
+        System.out.println("version 1");
+    }
+    
+}
